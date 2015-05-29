8da60abbf9182d104ff6981d740004cf93d1acc5
Caio Mariano <substancex@gmail.com>
2015-05-28 22:40:31 -0300

    alterando o relatorio

--- view/relTotal.php
+++ view/relTotal.php
@@ -1,9 +1,28 @@
 <?php require_once '../template/topo.php'; ?>
 <?php require_once '../template/menu.php'; ?>
-
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.0/css/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css">
 <div class="container">
     <h3>Relatório Geral</h3>
-    
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label for="dataInicio" class="col-xs-2 control-label">Data Inicio</label>
+        <div class="col-xs-6 input-group date">
+          <input type="text" class="form-control" name="dataInicio" id="dataInicio" placeholder="Data Inicio"><span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="dataFim" class="col-xs-2 control-label">Data Fim</label>
+        <div class="col-xs-6 input-group date">
+          <input type="text" class="form-control" id="dataFim" name="dataFim" placeholder="Data Fim"><span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-6 col-xs-2 text-right">
+          <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-filter"></span></button>
+        </div>
+      </div>
+    </form>
+    <hr>
     <?php if (count($lista) > 0) { ?>
     <table class="table table-striped table-hover" id="tableRelTotal">
       <thead>
@@ -40,7 +59,16 @@
     </div>
   <?php } ?>
 </div>
-
 </div>
-
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.input-group.date').datepicker({
+      format: "dd/mm/yyyy",
+      language: "pt-BR",
+      autoclose: true
+    });
+  });
+</script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.0/js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.0/locales/bootstrap-datepicker.pt-BR.min.js"></script>
 <?php require_once '../template/rodape.php'; ?>
