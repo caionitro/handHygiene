d0319f1f7f523d596674bea6cc18b3e79c7a4a2b
Rafael Ambrosio <rafaelambrosio.mail@gmail.com>
2017-09-28 08:30:48 -0300

    Update Lista.php

--- model/Lista.php
+++ model/Lista.php
@@ -1,10 +1,13 @@
 <?php
   class Lista
   {
+    //Transforma data do usuário em data para salvar no banco
     public static function dataBd($data){
       $dataF = explode('/', $data);
       return $dataF[2].'-'.$dataF[1].'-'.$dataF[0];
     }
+    
+    //transforma data do banco para data apresentável para o usuário
     public static function dataUser($data){
       $dataF = explode('-', $data);
       return $dataF[2].'/'.$dataF[1].'/'.$dataF[0];
